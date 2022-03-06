43057b0aff2095d3419d1ad4bdb7f5218f59ab09
Mike Crowe <drmikecrowe@gmail.com>
2022-03-06 11:33:36 -0500

    Updating progress bar/sorting of array

--- analyze.ts
+++ analyze.ts
@@ -12,11 +12,11 @@ const log_rank = debug("letters:rank");
 const log_avail = debug("letters:avail");
 const log = debug("words");
 
-const allLetters: Record<string, Letter> = ALL_LETTERS.reduce((acc: any, letter: string) => {
-  acc[letter] = new Letter(letter);
-  return acc;
-}, {});
-const letterList: Letter[] = Object.values(allLetters);
+const letterMap: Record<string, Letter> = {};
+ALL_LETTERS.forEach((letter) => {
+  letterMap[letter] = new Letter(letter);
+});
+const letterList: Letter[] = Object.values(letterMap);
 const wordList = Object.keys(masterWordList);
 
 let sortedLetters: string[] = [];
@@ -32,9 +32,9 @@ function DetailRank(word: string, offset: number): { rank: number; detail: any }
   let posHits = 0;
   let rank = 0;
   for (const [pos, letter] of letters.entries()) {
-    detail[`W${offset}L${pos}-Rank`] = allLetters[letter].rank;
-    let r = allLetters[letter].rank;
-    if (pos === allLetters[letter].bestPosition) {
+    detail[`W${offset}L${pos}-Rank`] = letterMap[letter].rank;
+    let r = letterMap[letter].rank;
+    if (pos === letterMap[letter].bestPosition) {
       detail[`W${offset}L${pos}-Pos`] = r;
       r *= 2;
       posHits++;
@@ -61,17 +61,17 @@ export function Process(): void {
 
     letters.forEach((letter, i) => {
       // For stats, use all words
-      allLetters[letter].bumpPosition(i);
+      letterMap[letter].bumpPosition(i);
 
       // Don't bother with words that have duplicate letters
-      if (letters.length === 5) allLetters[letter].addWord(word);
+      if (letters.length === 5) letterMap[letter].addWord(word);
     });
   });
   ALL_LETTERS.forEach((letter) => {
-    allLetters[letter].rank = allLetters[letter].occurrences / totalLetters;
+    letterMap[letter].rank = letterMap[letter].occurrences / totalLetters;
   });
 
-  letterList.sort((a, b) => b.rank - a.rank);
+  letterList.sort((a, b) => b.occurrences - a.occurrences);
   sortedLetters = letterList.map((l) => l.letter);
   bestLetters = sortedLetters.slice(0, 15);
   log_verbose(`Best letters: ${bestLetters.join(",")}`);
@@ -79,7 +79,7 @@ export function Process(): void {
     bestLetters
       .map(
         (letter, i) =>
-          `${letter} - ${i + 1}: rank=${(100 * allLetters[letter].rank).toFixed(1)}%, pos=${allLetters[letter].bestPosition + 1}`,
+          `${letter} - ${i + 1}: rank=${(100 * letterMap[letter].rank).toFixed(1)}%, pos=${letterMap[letter].bestPosition + 1}`,
       )
       .join("\n"),
   );
@@ -110,14 +110,16 @@ export function buildCombinations(): string[] {
   const results: Results[] = [];
   const details: any[] = [];
 
-  const keyToRank = (words: string[]): number => Rank(words[0]) + Rank(words[1]) + Rank(words[2]);
+  const ranks = [0.6, 0.25, 0.15];
 
   const word1entries = [...Available().entries()]; // bestLetters.slice(8)
 
-  const bar = new ProgressBar(":percent complete [:bar] :show :etas (:total total)", { total: word1entries.length });
+  const bar = new ProgressBar(":percent [:bar] :etas", { total: word1entries.length });
 
-  for (const [_, word1] of word1entries) {
+  for (const [i1, word1] of word1entries) {
     const word2entries = [...Available(toLetterAry(word1)).entries()];
+    if (i1 === 0) bar.interrupt(`Total Loops: ${word1entries.length}`);
+    bar.tick(0);
     for (const [i2, word2] of word2entries) {
       for (const word3 of [...Available(toLetterAry(word1 + word2))]) {
         const words = [word1, word2, word3];
@@ -128,15 +130,15 @@ export function buildCombinations(): string[] {
         const { rank: word3rank, detail: detail3 } = DetailRank(word3, 3);
         const detail = {
           word1,
-          word1rank,
+          word1rank: word1rank * ranks[0],
           ...detail1,
           word2,
-          word2rank,
+          word2rank: word2rank * ranks[1],
           ...detail2,
           word3,
-          word3rank,
+          word3rank: word3rank * ranks[2],
           ...detail3,
-          combRank: word1rank * 0.6 + word2rank * 0.3 + word3rank * 0.1,
+          combRank: word1rank * ranks[0] + word2rank * ranks[1] + word3rank * ranks[2],
           posHits: detail1["W1-posHits"] + detail2["W2-posHits"] + detail3["W3-posHits"],
         };
         details.push(detail);
@@ -145,22 +147,19 @@ export function buildCombinations(): string[] {
           words,
           rank: detail.combRank,
           posHits: detail.posHits,
-          output: `${key}: rank=${detail.combRank.toFixed(1)}, posHits=${detail.posHits}`,
+          output: `${key}: rank=${detail.combRank.toFixed(2)}, ${detail.word1rank.toFixed(2)}/${detail.word2rank.toFixed(
+            2,
+          )}/${detail.word3rank.toFixed(2)}, posHits=${detail.posHits}`,
         };
         let found = false;
-        const myRank = keyToRank(words);
-        for (let [i, existing] of results.entries()) {
-          if (myRank > existing.rank) {
+        for (const [i, existing] of results.entries()) {
+          if (detail.combRank > existing.rank) {
             results.splice(i, 0, result);
             found = true;
             break;
           }
         }
         if (!found) results.push(result);
-        bar.tick(0, {
-          total: results.length,
-          show: `${i2}/${word2entries.length} 2nd`,
-        });
         writeFileSync("./all.json", JSON.stringify(results.map((r) => r.output).slice(0, 10), null, 4));
         writeFileSync("./details.json", JSON.stringify(details, null, 4));
       }
